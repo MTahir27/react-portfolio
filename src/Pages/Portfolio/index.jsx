698bac1ad798176f0ab7927252f804ed4b22f574
MTahir27 <m.tahiridrees27@gmail.com>
2022-02-26 10:06:28 +0500

    Portfolio Card Generated

--- src/Pages/Portfolio/index.jsx
+++ src/Pages/Portfolio/index.jsx
@@ -1,13 +1,15 @@
-import * as React from 'react';
-import Box from '@mui/material/Box';
-import Grid from '@mui/material/Grid';
-import Portfolio from '../../Components/Portfolio';
+import * as React from "react";
+import Box from "@mui/material/Box";
+import Grid from "@mui/material/Grid";
+import PortfolioCard from "../../Components/PortfolioCard";
 
-export default function PorfolioPage(){
+export default function PorfolioPage() {
   return (
     <Box sx={{ flexGrow: 1 }}>
       <Grid container spacing={2}>
-        <Portfolio />
+        <Grid item xs={12} sm={6} md={4} lg={3}>
+          <PortfolioCard />
+        </Grid>
       </Grid>
     </Box>
   );
