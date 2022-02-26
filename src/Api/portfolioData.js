95ada7b9a4254a481243db3ad6bd617524e58a2d
MTahir27 <m.tahiridrees27@gmail.com>
2022-02-26 14:31:37 +0500

    PortfolioData js Added

---
+++ src/Api/portfolioData.js
@@ -0,0 +1,43 @@
+const portfolioArray = [{
+        name: "Project Name",
+        desc: "Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging across all continents except Antarctica",
+        img: "https://mui.com/static/images/cards/contemplative-reptile.jpg",
+        url: "https://mui.com/static/images/cards/contemplative-reptile.jpg",
+        type: "Ecommerce App",
+        id: 1,
+    },
+    {
+        name: "Project Name",
+        desc: "Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging across all continents except Antarctica",
+        img: "https://mui.com/static/images/cards/contemplative-reptile.jpg",
+        url: "https://mui.com/static/images/cards/contemplative-reptile.jpg",
+        type: "Ecommerce App",
+        id: 2,
+    },
+    {
+        name: "Eduaction Website",
+        desc: "Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging across all continents except Antarctica",
+        img: "https://mui.com/static/images/cards/contemplative-reptile.jpg",
+        url: "https://mui.com/static/images/cards/contemplative-reptile.jpg",
+        type: "Eduaction Website",
+        id: 3,
+    },
+    {
+        name: "LMS Website",
+        desc: "Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging across all continents except Antarctica",
+        img: "https://mui.com/static/images/cards/contemplative-reptile.jpg",
+        url: "https://mui.com/static/images/cards/contemplative-reptile.jpg",
+        type: "LMS Website",
+        id: 4,
+    },
+    {
+        name: "Real Estate App",
+        desc: "Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging across all continents except Antarctica",
+        img: "https://mui.com/static/images/cards/contemplative-reptile.jpg",
+        url: "https://mui.com/static/images/cards/contemplative-reptile.jpg",
+        type: "Real Estate App",
+        id: 5,
+    },
+];
+
+export default portfolioArray;
