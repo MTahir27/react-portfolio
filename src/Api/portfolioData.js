d90867e2dcf5141dace5c8ae301c46896ee62245
MTahir27 <m.tahiridrees27@gmail.com>
2022-02-26 15:47:20 +0500

    Url Chnage

--- src/Api/portfolioData.js
+++ src/Api/portfolioData.js
@@ -3,55 +3,55 @@ import technolosft from "../Assets/Images/Portfolio/technolosoft.png";
 import avanus from "../Assets/Images/Portfolio/avenus.png";
 import loopro from "../Assets/Images/Portfolio/loopro.png";
 import ecommerceWatch from "../Assets/Images/Portfolio/ecommerce-watch.png";
-import ecommercePlant from "../Assets/Images/Portfolio/ecommerce-plant.png"
+import ecommercePlant from "../Assets/Images/Portfolio/ecommerce-plant.png";
 const portfolioArray = [{
-        name: "Javascript Projects",
-        desc: "Basic level JavaScript projects created & linked in this project.",
-        img: javscriptProject,
-        url: "https://mt-javascript-projects.herokuapp.com/",
-        type: "javascript",
-        id: 1,
-    },
-    {
-        name: "Technolosft",
-        desc: "This website is the IT Company website.",
-        img: technolosft,
-        url: "http://technolosoft.com/",
-        type: "html",
-        id: 2,
-    },
-    {
-        name: "Avauns Website",
-        desc: "This is single page website created with html, css, bootsrap for client",
-        img: avanus,
-        url: "https://avenuesecomwebsite.herokuapp.com/index.html",
-        type: "html",
-        id: 3,
-    },
-    {
-        name: "Loopro Website",
-        desc: "This website is developed in React with Tailwind CSS for client",
-        img: loopro,
-        url: "https://mt-loopro.netlify.app/",
-        type: "react",
-        id: 4,
-    },
-    {
-        name: "Ecommerce Website",
-        desc: "This is ecommerce website that sales watches.",
-        img: ecommerceWatch,
-        url: "https://mtahir27.github.io/EcommerceWebsite/",
-        type: "html",
-        id: 5,
-    },
-    {
-        name: "Ecommerce Website",
-        desc: "This is ecommerce website that sales watches. This website is developed with the help youtube tutorial.",
-        img: ecommercePlant,
-        url: "https://github.com/MTahir27/Plant-Website/tree/gh-pages",
-        type: "html",
-        id: 5,
-    },
+    name: "Javascript Projects",
+    desc: "Basic level JavaScript projects created & linked in this project.",
+    img: javscriptProject,
+    url: "https://mt-javascript-projects.herokuapp.com/",
+    type: "javascript",
+    id: 1,
+  },
+  {
+    name: "Technolosft",
+    desc: "This website is the IT Company website.",
+    img: technolosft,
+    url: "http://technolosoft.com/",
+    type: "html",
+    id: 2,
+  },
+  {
+    name: "Avauns Website",
+    desc: "This is single page website created with html, css, bootsrap for client",
+    img: avanus,
+    url: "https://avenuesecomwebsite.herokuapp.com/index.html",
+    type: "html",
+    id: 3,
+  },
+  {
+    name: "Loopro Website",
+    desc: "This website is developed in React with Tailwind CSS for client",
+    img: loopro,
+    url: "https://mt-loopro.netlify.app/",
+    type: "react",
+    id: 4,
+  },
+  {
+    name: "Ecommerce Website",
+    desc: "This is ecommerce website that sales watches.",
+    img: ecommerceWatch,
+    url: "https://mtahir27.github.io/EcommerceWebsite/",
+    type: "html",
+    id: 5,
+  },
+  {
+    name: "Ecommerce Website",
+    desc: "This is ecommerce website that sales watches. This website is developed with the help youtube tutorial.",
+    img: ecommercePlant,
+    url: "https://mtahir27.github.io/Plant-Website/",
+    type: "html",
+    id: 5,
+  },
 ];
 
 export default portfolioArray;
--- src/Pages/Portfolio/index.jsx
+++ src/Pages/Portfolio/index.jsx
@@ -9,7 +9,7 @@ export default function PorfolioPage() {
   return (
     <Box sx={{ flexGrow: 1 }}>
       <Grid container spacing={2}>
-        {portfolioArray.map(({id, ...otherPortfolioProps}) => {
+        {portfolios.map(({id, ...otherPortfolioProps}) => {
           return (
             <Grid item xs={12} sm={6} md={4} lg={3} key={id}>
               <PortfolioCard  {...otherPortfolioProps} />
