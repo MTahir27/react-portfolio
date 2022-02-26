29f4a23efbc4d2de9f111ec7aa6a6029ad6307e9
MTahir27 <m.tahiridrees27@gmail.com>
2022-02-26 11:27:44 +0500

    Project Array Added

--- src/Pages/Portfolio/index.jsx
+++ src/Pages/Portfolio/index.jsx
@@ -4,18 +4,51 @@ import Grid from "@mui/material/Grid";
 import PortfolioCard from "../../Components/PortfolioCard";
 
 export default function PorfolioPage() {
+  const portfolioArray = [
+    {
+      name: "Project Name",
+      desc: "Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging across all continents except Antarctica",
+      img: "https://mui.com/static/images/cards/contemplative-reptile.jpg",
+      url: "https://mui.com/static/images/cards/contemplative-reptile.jpg",
+      type: "Ecommerce App",
+      id: "1",
+    },
+    {
+      name: "Project Name",
+      desc: "Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging across all continents except Antarctica",
+      img: "https://mui.com/static/images/cards/contemplative-reptile.jpg",
+      url: "https://mui.com/static/images/cards/contemplative-reptile.jpg",
+      type: "Ecommerce App",
+      id: "1",
+    },
+    {
+      name: "Eduaction Website",
+      desc: "Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging across all continents except Antarctica",
+      img: "https://mui.com/static/images/cards/contemplative-reptile.jpg",
+      url: "https://mui.com/static/images/cards/contemplative-reptile.jpg",
+      type: "Eduaction Website",
+      id: "3",
+    },
+    {
+      name: "LMS Website",
+      desc: "Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging across all continents except Antarctica",
+      img: "https://mui.com/static/images/cards/contemplative-reptile.jpg",
+      url: "https://mui.com/static/images/cards/contemplative-reptile.jpg",
+      type: "LMS Website",
+      id: "4",
+    },
+    {
+      name: "Real Estate App",
+      desc: "Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging across all continents except Antarctica",
+      img: "https://mui.com/static/images/cards/contemplative-reptile.jpg",
+      url: "https://mui.com/static/images/cards/contemplative-reptile.jpg",
+      type: "Real Estate App",
+      id: "5",
+    },
+  ];
   return (
     <Box sx={{ flexGrow: 1 }}>
       <Grid container spacing={2}>
-        <Grid item xs={12} sm={6} md={4} lg={3}>
-          <PortfolioCard />
-        </Grid>
-        <Grid item xs={12} sm={6} md={4} lg={3}>
-          <PortfolioCard />
-        </Grid>
-        <Grid item xs={12} sm={6} md={4} lg={3}>
-          <PortfolioCard />
-        </Grid>
         <Grid item xs={12} sm={6} md={4} lg={3}>
           <PortfolioCard />
         </Grid>
