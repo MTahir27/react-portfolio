0dc8326a514d6cf751013cf5d71982932f1e55cc
MTahir27 <m.tahiridrees27@gmail.com>
2022-02-26 14:32:48 +0500

    Link Portfolio Arrayt

--- src/Pages/Portfolio/index.jsx
+++ src/Pages/Portfolio/index.jsx
@@ -2,8 +2,10 @@ import * as React from "react";
 import Box from "@mui/material/Box";
 import Grid from "@mui/material/Grid";
 import PortfolioCard from "../../Components/PortfolioCard";
+import portfolioArray from "../../Api/portfolioData";
 
 export default function PorfolioPage() {
+    const portfolios = portfolioArray;
   return (
     <Box sx={{ flexGrow: 1 }}>
       <Grid container spacing={2}>
