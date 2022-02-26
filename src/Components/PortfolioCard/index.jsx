c79905d6abf844d608eb4114491cb0b1da016e22
MTahir27 <m.tahiridrees27@gmail.com>
2022-02-26 10:16:55 +0500

    Link Styling

--- src/Components/PortfolioCard/index.jsx
+++ src/Components/PortfolioCard/index.jsx
@@ -1,17 +1,17 @@
-import * as React from 'react';
-import Card from '@mui/material/Card';
-import CardActions from '@mui/material/CardActions';
-import CardContent from '@mui/material/CardContent';
-import CardMedia from '@mui/material/CardMedia';
-import Button from '@mui/material/Button';
-import Typography from '@mui/material/Typography';
+import * as React from "react";
+import Card from "@mui/material/Card";
+import CardActions from "@mui/material/CardActions";
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
+import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
+import Link from "@mui/material/Link";
 
 export default function PortfolioCard() {
   return (
     <Card>
       <CardMedia
         component="img"
-        // height="140"
         image="https://mui.com/static/images/cards/contemplative-reptile.jpg"
         alt="green iguana"
       />
@@ -25,7 +25,11 @@ export default function PortfolioCard() {
         </Typography>
       </CardContent>
       <CardActions>
-        <Button size="small">Go to website</Button>
+        <Button size="small">
+          <Link href="#" color="inherit" underline="none">
+            Go to website
+          </Link>
+        </Button>
       </CardActions>
     </Card>
   );
