4e11f214d8aadbf2dc012fb6fc270aedc18f3074
MTahir27 <m.tahiridrees27@gmail.com>
2022-03-09 07:48:10 +0500

    Portfolio Added

--- src/Api/portfolioData.js
+++ src/Api/portfolioData.js
@@ -4,7 +4,16 @@ import avanus from "../Assets/Images/Portfolio/avenus.png";
 import loopro from "../Assets/Images/Portfolio/loopro.png";
 import ecommerceWatch from "../Assets/Images/Portfolio/ecommerce-watch.png";
 import ecommercePlant from "../Assets/Images/Portfolio/ecommerce-plant.png";
-const portfolioArray = [{
+import ecommerceFurniture from "../Assets/Images/Portfolio/ecommerce-furniture.png";
+import ecommerceFurniture2 from "../Assets/Images/Portfolio/ecommerce-furniture-2.png";
+import education from "../Assets/Images/Portfolio/education.png";
+import documentation from "../Assets/Images/Portfolio/documentation.png";
+import documentation2 from "../Assets/Images/Portfolio/documentation-2.png";
+import realestate from "../Assets/Images/Portfolio/real-estate.png";
+import realestate2 from "../Assets/Images/Portfolio/real-estate-2.png";
+import realestate3 from "../Assets/Images/Portfolio/real-estate-3.png";
+const portfolioArray = [
+  {
     name: "Javascript Projects",
     desc: "Basic level JavaScript projects created & linked in this project.",
     img: javscriptProject,
@@ -51,6 +60,70 @@ const portfolioArray = [{
     url: "https://mtahir27.github.io/Plant-Website/",
     type: "html",
     id: 5,
+  },
+  {
+    name: "Ecommerce Website",
+    desc: "This is ecommerce website that sales Furnitures.",
+    img: ecommerceFurniture,
+    url: "https://mt-ecommerce-furniture.netlify.app/",
+    type: "html",
+    id: 6,
+  },
+  {
+    name: "Ecommerce Website",
+    desc: "This is ecommerce website that sales Furnitures.",
+    img: ecommerceFurniture2,
+    url: "https://mt-ecommerce-furniture-2.netlify.app/",
+    type: "html",
+    id: 7,
+  },
+  {
+    name: "Education Website",
+    desc: "This is education website for educational institues to teach students.",
+    img: education,
+    url: "https://mt-education-website.netlify.app/",
+    type: "html",
+    id: 8,
+  },
+  {
+    name: "Documentation Website",
+    desc: "This is documentation website for represtion website detail to other person.",
+    img: documentation,
+    url: "https://mt-documentation.netlify.app/",
+    type: "html",
+    id: 9,
+  },
+  {
+    name: "Documentation Website",
+    desc: "This is documentation website for represtion website detail to other person.",
+    img: documentation2,
+    url: "https://mt-documentation-2.netlify.app/",
+    type: "html",
+    id: 10,
+  },
+  {
+    name: "Real Estate Website",
+    desc: "This is real estate website for selling & purchasing property.",
+    img: realestate,
+    url: "https://mt-realestate.netlify.app/",
+    type: "html",
+    id: 11,
+  },
+  {
+    name: "Real Estate Website",
+    desc: "This is real estate website for selling & purchasing property.",
+    img: realestate2,
+    url: "https://mt-realestate-2.netlify.app/",
+    type: "html",
+    id: 12,
+  },
+  {
+    name: "Real Estate Website",
+    desc: "This is real estate website for selling & purchasing property.",
+    img: realestate3,
+    url: "https://mt-realestate-3.netlify.app/",
+    type: "html",
+    id: 13,
   },
 ];
 
